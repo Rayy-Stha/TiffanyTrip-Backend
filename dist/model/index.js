f515170a7ab7c2c63e430f5a69c7b40d182826cf
rayy_stha <remanstha10@gmail.com>
2026-04-15 00:07:51 +0545

    Prisma error

--- dist/model/index.js
+++ dist/model/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const prisma_1 = require("../../generated/prisma");
+const client_1 = require("@prisma/client");
 const pg_1 = require("pg");
 const adapter_pg_1 = require("@prisma/adapter-pg");
 const connectionString = process.env.DATABASE_URL;
@@ -9,6 +9,6 @@ if (!connectionString) {
 }
 const pool = new pg_1.Pool({ connectionString });
 const adapter = new adapter_pg_1.PrismaPg(pool);
-const prisma = new prisma_1.PrismaClient({ adapter });
+const prisma = new client_1.PrismaClient({ adapter });
 exports.default = prisma;
 //# sourceMappingURL=index.js.map
